e664f34b4bf7e00b38ea0bcaff370bab0d6c47d9
Lejian Luo <luolejian@outlook.com>
2023-06-14 15:02:00 -0400

    router scroll to top

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import Homapge from "./component/Homepage/Homepage";
 import Header from "./component/Header";
 import Footer from "./component/Footer";
 import Slider from "./component/Slider";
-import { Route,Routes} from "react-router-dom";
+import { Route,Routes, useLocation} from "react-router-dom";
 import SubMenu from "./component/SubMenu/SubMenu";
 import CustomizePage from "./component/CustomizeFood/CustomizePage";
 import Cart from "./component/Cart"
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import Checkout from "./component/Checkout/CheckoutPage";
 import Final from "./component/Checkout/FinalPage"
 
@@ -14,6 +14,11 @@ import Final from "./component/Checkout/FinalPage"
 export const CartContext=createContext();
 function App() {
   const [cartItems, setCart]=useState([]);
+  const location=useLocation();
+  
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
   
   return (
     <div className="App  font-verdana font-bold">
